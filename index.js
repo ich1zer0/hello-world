9070032b46af8ce989b100ab39f8c46c6a30c0c2
徳永貴文 <proton.ich1@protonmail.com>
2021-05-08 11:00:33 +0900

    Add DOMPurify 追加

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-module.exports = function fizzbuzz(value) {
-  if (value % 15 === 0) {
-    return "FizzBuzz"
-  }
-  if (value % 3 === 0) {
-    return "Fizz"
-  }
-  if (value % 5 === 0) {
-    return "Buzz"
-  }
-  return String(value)
-}
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+console.log(DOMPurify.sanitize('<img src=x onerror=alert(1)//>'));
+console.log(DOMPurify.sanitize('<svg><g/onload=alert(2)//<p>'));
+console.log(DOMPurify.sanitize('<p>abc<iframe//src=jAva&Tab;script:alert(3)>def</p>'));
+console.log(DOMPurify.sanitize('<math><mi//xlink:href="data:x,<script>alert(4)</script>">'));
+console.log(DOMPurify.sanitize('<TABLE><tr><td>HELLO</tr></TABL>'));
+console.log(DOMPurify.sanitize('<UL><li><A HREF=//google.com>click</UL>'));
